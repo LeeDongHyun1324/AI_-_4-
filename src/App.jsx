fdb094cc1745f1d918b09165bd5a6708b1460cd6
leedonghyun <lgf838738@gmail.com>
2026-05-22 15:57:28 +0900

    BookEditPage_뼈대

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import HomePage from './pages/HomePage';
+import BookEditPage from './pages/BookEditPage';
 // import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    // <HomePage />
+    <BookEditPage/>
   )
 }
 
