edea40364038f13345ce2417de567c6dc9e2ae74
leedonghyun <lgf838738@gmail.com>
2026-05-22 16:35:30 +0900

    NavBar_뼈대

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import "./App.css";
 import HomePage from './pages/HomePage';
 import BookEditPage from './pages/BookEditPage';
+import Navbar from './components/Navbar';
+
 // import './App.css'
 
 function App() {
   return (
-    <HomePage />
-    //<BookEditPage/>
+    <>
+      <Navbar />
+      <HomePage />
+      <BookEditPage/>
+    </>
   )
 }
 
