79e5d6dcce8dc16817d5ad2d0909f13632a15e9c
kssky98 <kssky98@naver.com>
2026-05-22 15:57:41 +0900

    BookCreatePage 예시2

--- mini-project/src/pages/BookCreatePage.jsx
+++ mini-project/src/pages/BookCreatePage.jsx
@@ -1,17 +1,19 @@
-function BookCreatePage() {function BookCreatePage */}
+function BookCreatePage() {
+  return (
+    <div>
+      <h1 className="page-title">새 도서 등록</h1>
+
       <input
         type="text"
         className="input-title"
         placeholder="도서명을 입력하세요"
       />
 
-      {/* 내용 입력 */}
       <textarea
         className="input-content"
         placeholder="도서의 내용을 입력하세요"
       />
 
-      {/* 버튼 영역 */}
       <button className="btn-cancel">취소</button>
       <button className="btn-submit">저장</button>
     </div>
@@ -19,7 +21,3 @@ function BookCreatePage() {function BookCreatePage */}
 }
 
 export default BookCreatePage;
-  return (
-    <div>
-      {/* 페이지 제목 */}
-      <h1 className="page-title">새 도서 등록</h1>
