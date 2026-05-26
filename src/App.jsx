fc6c62aee922a9729c60ff3aa58c7078b595fa93
leedonghyun <lgf838738@gmail.com>
2026-05-26 11:20:47 +0900

    app버튼 처리

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,16 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import BookListPage from './pages/BookListPage';
 import BookEditPage from './pages/BookEditPage';
-// import BookListPage from "./pages/BookListPage";
-// import BookCreatePage from "./pages/BookCreatePage";
 
 function App() {
+  const [page, setPage] = useState('home');
+
   return (
     <>
-      <Navbar />
-      <HomePage />
-      <BookEditPage/>
+      <Navbar onNavigate={setPage} />
+      {page === 'home' && <HomePage onNavigate={setPage} />}
+      {page === 'list' && <BookListPage />}
+      {page === 'edit' && <BookEditPage />}
     </>
   );
 }
