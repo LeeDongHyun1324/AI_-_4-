d3f7aa8ed66ef5c788f4bb58c5ea24705fbcf291
yujin <hanycorea@naver.com>
2026-05-26 13:11:44 +0900

    커밋

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,20 @@ import "./App.css";
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import BookListPage from './pages/BookListPage';
-import BookEditPage from './pages/BookEditPage';
+import BookCreatePage from './pages/BookCreatePage';
 
 function App() {
   const [page, setPage] = useState('home');
 
   return (
     <>
-<<<<<<< HEAD
-      <Navbar />
-      <HomePage />
-      <BookEditPage/>
-
-    </>
-  )
-
-}
-
-export default App;
-=======
       <Navbar onNavigate={setPage} />
       {page === 'home' && <HomePage onNavigate={setPage} />}
       {page === 'list' && <BookListPage />}
-      {page === 'edit' && <BookEditPage />}
+      {page === 'edit' && <BookCreatePage />}
     </>
   );
 }
 
 export default App;
->>>>>>> ffe3fa40bea1187099d30890ea03702ed430d9eb
+
