fbf4093f493f86952e5d9549ebdff40d3200be2e
leedonghyun <lgf838738@gmail.com>
2026-05-26 11:13:36 +0900

    Navbar 이벤트 처리

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,38 @@ import { useState } from 'react'
 import "./App.css";
 import HomePage from './pages/HomePage';
 import BookEditPage from './pages/BookEditPage';
+// import BookListPage from "./pages/BookListPage";
+// import BookCreatePage from "./pages/BookCreatePage";
 import Navbar from './components/Navbar';
 
 // import './App.css'
 
 function App() {
+  // 페이지 이동 useState
+  const [page, setPage] = useState("home");
+
+  const renderPage = () => {
+    switch (page) {
+      case "home":
+        return <HomePage />;
+
+      case "list":
+        return <BookListPage />;
+
+      case "create":
+        return <BookCreatePage />;
+
+      default:
+        return <HomePage />;
+    }
+  };
+
   return (
     <>
-      <Navbar />
-      <HomePage />
-      <BookEditPage/>
+      <Navbar setPage={setPage} />
+      {renderPage()}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,32 @@
-function Navbar() {
+function Navbar({ setPage }) {
   return (
     <nav className="navbar">
       <h1 className="header">도서관리</h1>
 
       <div className="nav-menu">
-        <button className="home">홈</button>
+        <button
+          className="home"
+          onClick={() => setPage("home")}
+        >
+          홈
+        </button>
 
-        <button className="list-header">
+        <button
+          className="list-header"
+          onClick={() => setPage("list")}
+        >
           도서 목록
         </button>
 
-        <button className="addBook">
+        <button
+          className="addBook"
+          onClick={() => setPage("create")}
+        >
           새 도서 등록
         </button>
       </div>
     </nav>
   );
 }
-
 
 export default Navbar;
